7c79eb2a28edc777f2a4958236a638134a53f8dc
gogojimmy <jimmy@gogojimmy.net>
2012-11-28 11:24:30 +0800

    Account method

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,6 +7,8 @@ class Account < ActiveRecord::Base
   after_create :build_create_comment
   after_update :build_update_comment
 
+  validates_numericality_of :balance, greater_than_or_equal_to: 0
+
   acts_as_commentable
 
   def created_user
@@ -15,6 +17,21 @@ class Account < ActiveRecord::Base
 
   def updated_user
     User.find(self.updated_by)
+  end
+
+  def withdraw(amount, user=nil)
+    adjust_balance_and_save(-amount, user)
+  end
+
+  def deposit(amount, user=nil)
+    adjust_balance_and_save(amount, user)
+  end
+
+  def self.transfer(from, to, amount, user=nil)
+    transaction do
+      from.withdraw(amount, user)
+      to.deposit(amount, user)
+    end
   end
 
   protected
@@ -27,6 +44,14 @@ class Account < ActiveRecord::Base
   def build_update_comment
     comment = Comment.build_from(self, self.updated_by, "#{self.updated_user.name}更新了#{self.name}</br>更新內容：#{self.changes}")
     comment.save
+  end
+
+  private
+
+  def adjust_balance_and_save(amount, user=nil)
+    self.updated_by ||= user
+    self.balance += amount
+    save!
   end
 
 end
