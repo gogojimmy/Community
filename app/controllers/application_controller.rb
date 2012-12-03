706fe8c4c9ab50c229e12e6cde0ea446d5c3b860
gogojimmy <jimmy@gogojimmy.net>
2012-12-04 02:09:30 +0800

    Add authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate_for_internal
   protect_from_forgery
+
+  def authenticate_for_internal
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'bravo' && password == '034013967'
+    end
+  end
 end
