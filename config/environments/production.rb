cdca1beab4d3190efcf001dd87edd7dfe14cd224
gogojimmy <jimmy@gogojimmy.net>
2012-12-11 22:13:27 +0800

    add uservoice and mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Community::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = { :host => 'http://bravo-gogojimmy.herokuapp.com' }
+
 end
 
 Community::Application.config.middleware.use ExceptionNotifier,
